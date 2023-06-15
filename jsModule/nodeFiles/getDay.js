98e1a6bf6aeb2740f2d44b584bfe9008d79b4f81
Tonyb650 <anthonylbrierly@gmail.com>
2023-06-14 20:52:34 -0700

    Added new JS assignments

---
+++ jsModule/nodeFiles/getDay.js
@@ -0,0 +1,10 @@
+var today = new Date();
+console.log(today);
+console.log(today.getDay());
+if(today.getDay() == 1) {
+    console.log("I hate Mondays!");
+}
+    
+if(today.getDay() != 1) {
+    console.log("Today is alright!");
+}
